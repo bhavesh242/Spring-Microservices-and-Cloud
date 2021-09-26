7b881f524d7256554692095c23a0c8b17d94acce
Bhavesh Agrawal <bhavesh.agrawal242@gmail.com>
2021-09-26 01:23:11 -0700

    Test Change

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersService.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersService.java
@@ -3,6 +3,5 @@ package com.appsdeveloperblog.photoapp.api.users.service;
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 
 public interface UsersService {
-
 	UserDto createUser(UserDto userDetails);
 }
